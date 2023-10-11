44c4d788edc8cbbaae951e4a9a66e82118a160aa
zschuah <zschuah@hotmail.com>
2023-10-11 17:48:28 +0800

    added zod

--- src/pages/DailyRandom.tsx
+++ src/pages/DailyRandom.tsx
@@ -1,36 +1,57 @@
-import cryptoRandomString from "crypto-random-string";
 import { useEffect, useState } from "react";
-import { PokeType } from "../store/slices/pokeSlice";
+import { z } from "zod";
 import { URL_POKEMON } from "../utils/constants";
 
+const pokeSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  sprites: z.object({
+    other: z.object({
+      dream_world: z.object({
+        front_default: z.string(),
+      }),
+    }),
+  }),
+  weight: z.number(),
+});
+
+type PokeSchemaType = z.infer<typeof pokeSchema>;
+
 const DailyRandom = () => {
-  const [dailyPoke, setDailyPoke] = useState<PokeType>(null!);
+  const [dailyPoke, setDailyPoke] = useState<PokeSchemaType | null>(null);
 
   const handleFetchPokemon = async () => {
     const res = await fetch(URL_POKEMON + 1);
     const data = await res.json();
 
-    const newPoke: PokeType = {
-      id: cryptoRandomString({ length: 6, type: "distinguishable" }),
-      pokeId: data.id,
-      name: data.name,
-      image: data.sprites.other.dream_world.front_default,
-    };
-
-    setDailyPoke(newPoke);
+    const validatedPoke = pokeSchema.safeParse(data);
+    if (validatedPoke.success) {
+      setDailyPoke(validatedPoke.data);
+    } else {
+      console.error(validatedPoke.error);
+    }
   };
 
   useEffect(() => {
     handleFetchPokemon();
   }, []);
 
-  return (
-    <div className="mt-8">
-      <h2>Daily Random Pokemon</h2>
-      <p>{dailyPoke?.name}</p>
-      <img src={dailyPoke?.image} alt={dailyPoke?.name} />
-    </div>
-  );
+  if (dailyPoke) {
+    return (
+      <div className="mt-8">
+        <h2>Daily Random Pokemon</h2>
+
+        <div>
+          <p>{dailyPoke.name}</p>
+          <p>{dailyPoke.weight}</p>
+          <img
+            src={dailyPoke.sprites.other.dream_world.front_default}
+            alt={dailyPoke.name}
+          />
+        </div>
+      </div>
+    );
+  }
 };
 
 export default DailyRandom;
