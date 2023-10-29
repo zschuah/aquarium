2c176df3320c4e30431a75e7688307fb81ea72bf
zschuah <zschuah@hotmail.com>
2023-10-29 20:48:19 +0800

    removed BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 import { store } from "./store/index.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
